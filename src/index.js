054972d59f7abfb85f05972f625c5efeacbae504
Morven Watt <morvenwatt@gmail.com>
2019-10-19 16:27:17 -0700

    added json server

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { 
-    faPencil, faFolder, faTrash, faStickyNote, faChevronLeft 
-} from '@fortawesome/free-solid-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { 
+//     faPencil, faFolder, faTrash, faStickyNote, faChevronLeft 
+// } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faPencil, faFolder, faTrash, faStickyNote, faChevronLeft)
+// library.add(faPencil, faFolder, faTrash, faStickyNote, faChevronLeft)
 
 ReactDOM.render(
 <BrowserRouter>
